eb3e1a13bcae7a9b0749617f55e0ae931c93db74
Adi <adithya25032004@gmail.com>
2025-09-21 12:12:56 +0530

    add initial API and model definitions for travel-related data

--- backend/models/api_models.py
+++ backend/models/api_models.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, field_validator, Field
 from datetime import date
 
 
-class Destination(BaseModel):
-    """Represents a travel destination query."""
+class Sights(BaseModel):
+    """Represents query for trip advisor api."""
     query: str = Field(..., description="What to see, e.g., 'Maldives temples'.")
 
 
@@ -11,8 +11,8 @@ class FlightSchedule(BaseModel):
     """Flight schedule details."""
     departure_airport_code: str = Field(..., min_length=3, max_length=3, description="3-letter IATA code")
     arrival_airport_code: str = Field(..., min_length=3, max_length=3, description="3-letter IATA code")
-    outbound_date: date
-    inbound_date: date
+    outbound_date: str   #YYYY-MM_DD
+    return_date: str #YYYY-MM-DD
 
 
 class HotelDetails(BaseModel):
@@ -26,3 +26,31 @@ class HotelDetails(BaseModel):
         if not 2 <= v <= 5:
             raise ValueError("hotel_class must be between 2 and 5")
         return v
+
+class SightsResponse(BaseModel):
+    """The response format for trip advisor api result"""
+    name: str
+    description: str
+    location: str
+    rating: float                            
+    link: str
+
+
+class FlightResponse(BaseModel):
+    """The Response format for flight api result"""
+    destination_airport: str
+    duration: int
+    stops: str
+    price: str  # in usd by default need to add it later
+    # airline: str    # this is for immediate airport if stops is not 0
+    # aeroplane: str
+
+
+class HotelResponse(BaseModel):
+    """The Response format for Sights api result"""
+    name: str
+    description: str
+    cost_per_night: str
+    rating: float
+    link: str 
+    

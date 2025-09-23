d8631af2cadd3460eeb17c3601541311d3771ad2
Adi <adithya25032004@gmail.com>
2025-09-23 22:30:52 +0530

    changed folder name

--- backend/modules/test_service_api.py
+++ backend/modules/test_service_api.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 sys.path.append('') 
 
 from backend.models.api_models import Sights, FlightSchedule, HotelDetails
-from backend.api.Service_Api import flight_schedules, hotel_list, tourist_attractions
+from backend.modules.Service_Api import flight_schedules, hotel_list, tourist_attractions
 
 
 # Calculate dates for the tests - using future dates
